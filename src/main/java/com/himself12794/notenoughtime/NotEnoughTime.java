0cc683b1c6586ae45f0f4e6fb8e43b39c3a2e226
Himself12794 <pwhiting.bba@gmail.com>
2017-10-26 22:02:00 -0400

    made it a server side mod

--- src/main/java/com/himself12794/notenoughtime/NotEnoughTime.java
+++ src/main/java/com/himself12794/notenoughtime/NotEnoughTime.java
@@ -13,7 +13,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = NotEnoughTime.MODID, version = NotEnoughTime.VERSION)
+//@Mod(modid = NotEnoughTime.MODID, version = NotEnoughTime.VERSION, acceptableRemoteVersions = "*")
 public class NotEnoughTime {
     public static final String MODID = "notenoughtime";
     public static final String VERSION = "1.0.0-pre1";
