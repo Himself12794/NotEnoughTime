590e6c10c12375925b58ffc2a2d9e36282c67847
Himself12794 <pwhiting.bba@gmail.com>
2017-10-27 20:15:57 -0400

    updated version

--- src/main/java/com/himself12794/notenoughtime/NotEnoughTime.java
+++ src/main/java/com/himself12794/notenoughtime/NotEnoughTime.java
@@ -20,7 +20,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 public class NotEnoughTime {
     public static final String MODID = "notenoughtime";
     public static final String NAME = "NotEnoughTime";
-    public static final String VERSION = "1.0.0-pre2";
+    public static final String VERSION = "1.0.0-pre3";
     
     @Mod.Instance
     private static NotEnoughTime INSTANCE;
